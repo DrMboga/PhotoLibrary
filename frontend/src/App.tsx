d424aa651817a3537882e9b68d45688bb781ebb5
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-10-25 13:45:48 +0200

    frontend: react-router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from './photos/HomePage';
+import SettingsPage from './settings/SettingsPage';
 
 function App() {
   return (
-    <>
-      <p>Hello there</p>
-    </>
+    <Router>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
+      </div>
+
+      {/*class Container*/}
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
