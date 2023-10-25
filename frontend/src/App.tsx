97ac293912d1148880dd03be0de6c7d7eac158d8
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-10-25 16:08:52 +0200

    frontend: mui added and top bar with menu rearranged

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,208 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './photos/HomePage';
 import SettingsPage from './settings/SettingsPage';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CameraIcon from '@mui/icons-material/Camera';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
+interface PageRoute {
+  key: string;
+  route: string;
+  displayName: string;
+  element: JSX.Element;
+  iconElement: JSX.Element;
+}
+
+const logo = 'Photos';
+const pages: PageRoute[] = [
+  {
+    key: 'home',
+    route: '/',
+    displayName: 'All Photos',
+    element: <HomePage />,
+    iconElement: <CollectionsIcon fontSize="small" />,
+  },
+  {
+    key: 'settings',
+    route: '/settings',
+    displayName: 'Settings',
+    element: <SettingsPage />,
+    iconElement: <SettingsIcon fontSize="small" />,
+  },
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function App() {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <Router>
-      <div>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-      </div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <CameraIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}></CameraIcon>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              {logo}
+            </Typography>
 
-      {/*class Container*/}
-      <div>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <Link to={page.route}>
+                    <MenuItem key={page.key} onClick={handleCloseNavMenu}>
+                      <ListItemIcon>{page.iconElement}</ListItemIcon>
+                      <Typography textAlign="center">{page.displayName}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+
+            <CameraIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }}></CameraIcon>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              {logo}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.key}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  href={page.route}
+                >
+                  <IconButton sx={{ color: 'white' }}>{page.iconElement}</IconButton>
+                  {page.displayName}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidth={false}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          {pages.map((page) => (
+            <Route path={page.route} element={page.element} />
+          ))}
         </Routes>
-      </div>
+      </Container>
     </Router>
   );
 }
