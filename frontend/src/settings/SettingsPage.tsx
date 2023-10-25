77203abdf6f991d4337132d28a25eaa858379113
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-10-25 13:27:14 +0200

    frontend: pages scaffold

---
+++ frontend/src/settings/SettingsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SettingsPage() {
+    return (
+        <>
+            <p>Hello there from settings page</p>
+        </>
+    );
+}
+
+export default SettingsPage;
