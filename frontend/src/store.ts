6f9c957a9b2e3ba60be669022bcf097336359e18
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-10-27 19:03:17 +0200

    frontend: app store persisted in local storage

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,11 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from './appSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Persist appReducer to the local storage via redux-persist
+const persistConfig = {
+  key: 'photo-lib',
+  version: 1,
+  storage,
+};
+
+const persistedAppReducer = persistReducer(persistConfig, appReducer);
 
 export const store = configureStore({
   reducer: {
-    app: appReducer,
+    app: persistedAppReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
