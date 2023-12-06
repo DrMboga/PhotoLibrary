6888bd87345f992efbd8e0ba6da4e7056281f70e
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-06 17:37:11 +0100

    chore: backend some importer service prepaprations

--- backend/PhotoLibraryBackend.MediaReader/ImporterService.cs
+++ backend/PhotoLibraryBackend.MediaReader/ImporterService.cs
@@ -10,11 +10,40 @@ public class ImporterService : IImporterService
         _logger = logger;
     }
 
-    public Task StartImport(string photoLibraryPath)
+    public async Task StartImport(string photoLibraryPath)
     {
         var files = Directory.GetFiles(photoLibraryPath);
         var directories = Directory.GetDirectories(photoLibraryPath);
         _logger.ImporterStartMessage(photoLibraryPath, files.Length, directories.Length);
-        return Task.CompletedTask;
+
+        int importedSuccessfully = 0;
+        foreach (var mediaFile in files)
+        {
+            var success = await ImportMediaFile(mediaFile);
+            if (success)
+            {
+                importedSuccessfully++;
+            }
+        }
+        // TODO: Log the folder name and importedSuccessfully count
+
+        // TODO: Find a way to read directories without recursion
+    }
+
+    private Task<bool> ImportMediaFile(string mediaFilePath)
+    {
+        try
+        {
+            // 1. figure out the media type by extension
+            // 2. Fill the media metadata according to media type
+            // 3. Create media thumbnail
+            // 4. Save media to DB
+            return Task.FromResult(true);
+        }
+        catch (Exception e)
+        {
+            // Write log message
+        }
+        return Task.FromResult(false);
     }
 }
