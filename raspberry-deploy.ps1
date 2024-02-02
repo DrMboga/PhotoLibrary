3cc79e5f669395ae19c2e5549b32501b6b8e504c
Mikhail Shabanov <dr.mboga@yahoo.com>
2024-02-02 17:07:28 +0100

    powershell script - rewrite appsettings json

--- raspberry-deploy.ps1
+++ raspberry-deploy.ps1
@@ -1,15 +1,12 @@
 class EnvironmentParameters {
     [string]$RaspberryAddress
     [string]$RaspberryPwd
-    [string]$KeycloakAuthServer
-    [string]$KeycloakRealm
-    [string]$KeycloakResource
-    [string]$KeycloakClientId
-    [string]$KeycloakRedirectUrl
     [string]$PhotoLibraryLocalPath
     [string]$PhotoLibraryLocalDeleteFolder
     [string]$PhotoDbConnectionString
+    [string]$IdentityDbConnectionString
     [string]$PhotoLibraryBackendUrl
+    [string]$PositionStackApiKey
 }
 
 function ReadParameters {
@@ -26,26 +23,6 @@ function ReadParameters {
             }
             'RASPBERRY_PWD' { 
                 $envParametersObject.RaspberryPwd = $keyValue[1]
-                continue
-            }
-            'KEYCLOAK_AUTH_SERVER' { 
-                $envParametersObject.KeycloakAuthServer = $keyValue[1]
-                continue
-            }
-            'KEYCLOAK_REALM' { 
-                $envParametersObject.KeycloakRealm = $keyValue[1]
-                continue
-            }
-            'KEYCLOAK_RESOURCE' { 
-                $envParametersObject.KeycloakResource = $keyValue[1]
-                continue
-            }
-            'KEYCLOAK_CLIENT_ID' { 
-                $envParametersObject.KeycloakClientId = $keyValue[1]
-                continue
-            }
-            'KEYCLOAK_REDIRECT_URL' { 
-                $envParametersObject.KeycloakRedirectUrl = $keyValue[1]
                 continue
             }
             'PHOTO_LIBRARY_LOCAL_PATH' { 
@@ -69,8 +46,25 @@ function ReadParameters {
                 $envParametersObject.PhotoDbConnectionString = $connectionString
                 continue
             }
+            'IDENTITY_DB_CONNECTION_STRING' { 
+                # Connection string has this syntax: Host=localhost;Database=photo;Username=postgres;Password=MyDocker6
+                # So, it is split and here we are assemble it back
+                $connectionString = ""
+                for ($i = 1; $i -lt $keyValue.Count; $i++) {
+                    $connectionString = $connectionString + $keyValue[$i]
+                    if ($i -lt $keyValue.Count-1) {
+                        $connectionString = $connectionString + "="
+                    }
+                }
+                $envParametersObject.IdentityDbConnectionString = $connectionString
+                continue
+            }
             'PHOTO_LIBRARY_BACKEND_URL' { 
                 $envParametersObject.PhotoLibraryBackendUrl = $keyValue[1]
+                continue
+            }
+            'POSITION_STACK_API_KEY' { 
+                $envParametersObject.PositionStackApiKey = $keyValue[1]
                 continue
             }
         }
@@ -79,6 +73,26 @@ function ReadParameters {
     return $envParametersObject
 }
 
+function ChangeBackendApplicationSettings {
+    param (
+        [EnvironmentParameters]$EnvironmentParams,
+        [string]$AppSettingsFileName
+    )
+    $AppSettingsContent = Get-Content -Path $AppSettingsFileName | Out-String | ConvertFrom-Json
+
+    $PhotoLobrarySectionContent = $AppSettingsContent.PhotoLibrary
+    $PhotoLobrarySectionContent.PhotoLibraryPath = $EnvironmentParams.PhotoLibraryLocalPath
+    $PhotoLobrarySectionContent.PhotoLibraryDeletedFolder = $EnvironmentParams.PhotoLibraryLocalDeleteFolder
+    $PhotoLobrarySectionContent.PositionStackApiKey = $EnvironmentParams.PositionStackApiKey
+
+    $ConnectionStringsSectionContent = $AppSettingsContent.ConnectionStrings
+    $ConnectionStringsSectionContent."photo-db" = $EnvironmentParams.PhotoDbConnectionString
+    $ConnectionStringsSectionContent."photo-identity-db" = $EnvironmentParams.IdentityDbConnectionString
+
+    $AppSettingsContent | ConvertTo-Json -Depth 100 | Out-File $AppSettingsFileName -Encoding utf8
+}
+
+#--Start--
 Clear-Host
 # Read parameters
 Write-Host "Reading deployment parameters..."
@@ -86,3 +100,4 @@ $EnvironmentParams = ReadParameters
 $EnvironmentParams
 
 #Read .\backend\PhotoLibraryBackend\appsettings.json
+ChangeBackendApplicationSettings $EnvironmentParams ".\backend\PhotoLibraryBackend\appsettings.json"
